df56b994200490fd6e5461a37d6c3c629f2f421a
Animesh Sharma <animeshsharma1996@gmail.com>
2021-04-17 02:21:01 +0100

    Health Logic

---
+++ NetworkTanks/Assets/_Scripts/CauseDamage.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CauseDamage : MonoBehaviour
+{
+    [SerializeField] private int maxDamage = 10;
+
+    public int GetDamage()
+    {
+        return maxDamage;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
